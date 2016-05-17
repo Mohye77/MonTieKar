44050338fd4d6a7a5db5e15407ad1db7096f7420
TiGruu <TiGruu@users.noreply.github.com>
2016-05-17 13:36:55 +0200

    map web api

---
+++ MonTieKar/Controllers/MapController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using MonTieKar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace MonTieKar.Controllers
+{
+    [Authorize]
+    public class MapController : ApiController
+    {
+        private ApplicationUserManager _userManager;
+
+        public MapController()
+        {
+        }
+
+        public MapController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET api/Map
+        public MapViewModel Get(GetRequest request)
+        {
+            //TODO: get criterias
+
+            // Map them
+            return new MapViewModel
+            {
+                MappedCriteria = new List<MappedCriteria>()
+                {
+                    new MappedCriteria
+                    {
+                        Coords = new Coordinates { Latitude = 0, Longitude = 0 },
+                        CriteriaNumber = 3
+                    }
+                }
+            };
+        }
+    }
+
+    public class GetRequest
+    {
+        public List<Filter> Filters { get; set; }
+    }
+
+    public class Filter
+    {
+        public string Name { get; set; }
+        public string Operator { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ MonTieKar/Models/MapViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MonTieKar.Models
+{
+    public class MapViewModel
+    {
+        public List<MappedCriteria> MappedCriteria { get; set; }
+    }
+
+    public class MappedCriteria
+    {
+        public Coordinates Coords { get; set; }
+        public int CriteriaNumber { get; set; }
+    }
+
+    public class Coordinates
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
