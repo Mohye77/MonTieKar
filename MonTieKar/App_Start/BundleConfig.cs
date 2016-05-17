5be97bdee687702d7fa1a1bdcffa9fe3252c1296
Mohye77 <mohye@orange.fr>
2016-05-17 13:12:48 +0200

    map

--- MonTieKar/App_Start/BundleConfig.cs
+++ MonTieKar/App_Start/BundleConfig.cs
@@ -29,6 +29,9 @@ namespace MonTieKar
                 "~/Scripts/app/home.viewmodel.js",
                 "~/Scripts/app/_run.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/map").Include(
+                "~/Scripts/map.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ MonTieKar/Scripts/map.js
@@ -0,0 +1,18 @@
+﻿var map;
+
+function initMap() {
+    var geocoder = new google.maps.Geocoder();
+    var location = "Paris, France";
+
+    map = new google.maps.Map(document.getElementById('map'), {});
+
+    geocoder.geocode({ 'address': location }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            map.setZoom(12,8);
+        } else {
+            alert("Could not find location: " + location);
+        }
+    });
+
+}
